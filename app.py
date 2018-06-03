95d479afca9b52e5938901eea5709c9d09ccbd1a
yoyok173 <saied.five@gmail.com>
2018-06-03 17:14:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -556,6 +556,42 @@ def handle_sticker_message(event):
         event.reply_token,
         sticker_message)
 
+@handler.add(PostbackEvent)
+def handle_postback(event):
+	if event.postback.data == 'update':
+		the_day = event.postback.params['date']
+		last_date = datetime.strptime(the_day,"%Y-%m-%d")
+		period = last_date + timedelta(days=28) #月經日期是第一天加上週期
+		preg = last_date + timedelta(days=10) #經期來後10天開始的一周內容易懷孕
+		diet = last_date + timedelta(days=7) #經期來後7天開始的一周內容易懷孕
+		bra = last_date + timedelta(days=17)
+
+		the_id = event.source.user_id
+		profile = line_bot_api.get_profile(the_id)
+		user_name = profile.display_name
+
+		content = ''
+		content += user_name + ' 你好~'
+		content += '已成功紀錄你最近來的日期' + event.postback.params['date'] + '\n\n'
+		content += '預計下一次差不多會是' + period.strftime("%m/%d") + '來 (σ`・∀・)σ\n\n'
+		content += preg.strftime("%m/%d") + ' 開始的一週很容易懷孕 (╯°Д°)╯ ┻━┻\n\n'
+		content += diet.strftime("%m/%d") + ' 開始的一週內少吃多動會瘦很快唷!!\n☜（ﾟ∀ﾟ☜）!!\n\n'
+		content += bra.strftime("%m/%d") + ' 開始的一週多按摩奶奶會長很大唷\n\n'
+
+
+		auth_json_path = 'google_sheet.json'
+		gss_scopes = ['https://spreadsheets.google.com/feeds']
+		gss_client = google_sheet.auth_gss_client(auth_json_path, gss_scopes)
+		spreadsheet_key = '1Q4hWEVjTB-rdc7HAi_Yc_cF4uymjfPHe70Cc36fLHyM'
+
+		google_sheet.update_sheet(gss_client, spreadsheet_key, the_id, the_day)
+		line_bot_api.reply_message(event.reply_token, TextSendMessage(text=content))
+
+
+
+	elif event.postback.data == 'contact_me':
+		line_bot_api.reply_message(event.reply_token, TextSendMessage(text='找我嗎(〃▽〃)?'))
+    
 
 if __name__ == '__main__':
     app.run()
