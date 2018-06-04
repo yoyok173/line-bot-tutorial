23d226a2598dafbde41512891c3a74deacf251c9
yoyok173 <saied.five@gmail.com>
2018-06-04 20:53:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -86,18 +86,14 @@ def handle_message(event):
                     "User_ID: " + profile.user_id + "\n" +
                     "Status: "+ str(profile.status_message))
             url = profile.picture_url
-            image_message = ImageSendMessage(
-                    original_content_url=url,
-                    preview_image_url=url
             )
             line_bot_api.reply_message(
                 event.reply_token,
-                TextSendMessage(result),
-                image_message
+                ImageSendMessage(
+                    original_content_url=url,
+                    preview_image_url=url
             )
-            #line_bot_api.reply_message(
-            #    event.reply_token, image_message)
-            #return 0
+            return 0
 
 @handler.add(MessageEvent, message=StickerMessage)
 def handle_sticker_message(event):
