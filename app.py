0d5a36b36b94c92e5153eba5683b5b8d33d37c77
yoyok173 <saied.five@gmail.com>
2018-06-07 13:41:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,6 +23,8 @@ from linebot.models import *
 app = Flask(__name__)
 config = configparser.ConfigParser()
 config.read("config.ini")
+translator = Translator()
+wiki_settings = {}
 
 line_bot_api = LineBotApi(config['line_bot']['Channel_Access_Token'])
 handler = WebhookHandler(config['line_bot']['Channel_Secret'])
